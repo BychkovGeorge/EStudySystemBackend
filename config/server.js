0884b8de8463eb1a1d50cfc4221bb18c2c16b0b7
g.bychkov <g.bychkov@grcc.tech>
2021-05-08 16:32:24 +0300

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'ea8dd75de6002030f8153ac3db6f4f40'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '00dfe722-5f0e-4b04-b4cd-7448363b1cbe'
+};
